1d4646b6c992d94cdeb2bd3258cb808e0328b8dc
Steve Albonico <salbonico@gmail.com>
2019-02-06 22:41:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190206223825_create_dogs.rb
+++ db/migrate/20190206223825_create_dogs.rb
@@ -1,8 +1,12 @@
 class CreateDogs.rb < ActiveRecord::Migration
   def up
+    create_table :dogs do |t|
+     t.string :name
+     t.string :breed
   end
 
   def down
+    drop_table :dogs
   end
 
 end
